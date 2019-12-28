0a76b03dde5f37f5bdbac5a0b09b319a9851cbe6
Diana V <d.valaityte.97@gmail.com>
2019-12-28 09:46:19 +0200

    Navbar prototype complete.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <v-app>
+            <div>
+                <v-toolbar>
+                    <v-toolbar-title>
+                        <a href="/">
+                            <v-img class="mr-3" src="@/assets/tl_logo.png" height="40px" width="50px"></v-img>
+                        </a>
+                    </v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items class="d-none d-md-block">
+                        <v-btn class="nav-btn" text v-for="(item, index) in menu"
+                               :key="index">{{ item.title }}</v-btn>
+                    </v-toolbar-items>
+
+                    <v-toolbar-items>
+                        <v-menu offset-y>
+                            <template v-slot:activator="{ on }">
+                                <v-btn class="d-block d-md-none"
+                                       v-on="on">
+                                    <v-icon class="material-icons">menu_open</v-icon>
+                                </v-btn>
+                            </template>
+                            <v-list>
+                                <v-list-item
+                                        v-for="(item, index) in menu"
+                                        :key="index">
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                        </v-menu>
+                    </v-toolbar-items>
+                </v-toolbar>
+            </div>
+        </v-app>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+    export default {
+        name: "app",
+        data: () => ({
+            menu: [
+                {title: 'about'},
+                {title: 'featured'},
+                {title: 'portfolio'},
+                {title: 'blog'},
+                {title: 'contact'},
+            ]
+        }),
     }
-  }
-}
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: 'Ubuntu Mono', monospace;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
+
+    .nav-btn {
+        font-size: 20px !important;
+        text-transform: none;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+
+Vue.use(vuetify);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: function (h) { return h(App) }
-}).$mount('#app')
+}).$mount('#app');
