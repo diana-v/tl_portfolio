4ee8227da3f196428edd5d5f9a9f6c42052f6c38
Diana V <d.valaityte.97@gmail.com>
2019-12-30 20:39:05 +0200

    Made the about page responsive to all screen sizes. Added a link to check credibility of certification.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,59 +1,14 @@
-<!--<template>-->
-<!--  <v-container grid-list-md text-md-center>-->
-<!--    <v-layout row wrap>-->
-<!--      <v-flex xs12>-->
-<!--        <v-card dark color="primary">-->
-<!--          <v-card-text class="px-0 header">about me</v-card-text>-->
-<!--        </v-card>-->
-<!--      </v-flex>-->
-<!--      <v-flex xs6>-->
-<!--        <v-card max-height="300px">-->
-<!--          <v-img src="@/assets/profie_photo.jpg" class="profile-img"-->
-<!--          />-->
-<!--        </v-card>-->
-<!--&lt;!&ndash;        <v-card dark color="primary">&ndash;&gt;-->
-
-<!--&lt;!&ndash;        </v-card>&ndash;&gt;-->
-<!--      </v-flex>-->
-<!--      <v-flex xs6>-->
-<!--        <v-card dark color="primary">-->
-<!--          <v-card-text class="px-0">Experience</v-card-text>-->
-<!--        </v-card>-->
-<!--      </v-flex>-->
-<!--      <v-flex xs6>-->
-<!--        <v-card dark color="primary">-->
-<!--          <v-card-text class="px-0">Name - description</v-card-text>-->
-<!--        </v-card>-->
-<!--      </v-flex>-->
-<!--      <v-flex xs6>-->
-<!--        <v-card dark color="primary">-->
-<!--          <v-card-text class="px-0">Education</v-card-text>-->
-<!--        </v-card>-->
-<!--      </v-flex>-->
-
-<!--    </v-layout>-->
-<!--  </v-container>-->
-<!--</template>-->
-
-<!--<style>-->
-<!--  .profile-img {-->
-<!--    object-fit: cover;-->
-<!--    width: 100%;-->
-<!--    max-height: 100%;-->
-<!--  }-->
-<!--</style>-->
-
 <template>
-  <div>
-  <header>
-  <h1 class="header">about me:</h1>
-  </header>
-  <div class="photo-section">
-    <v-img src="@/assets/profie_photo.jpg" class="profile-img"/>
-  </div>
-    <pre class="description-section">My name is Tomas. I am a Dev-Ops Engineer based in Lithuania. I am a seasoned expert that has introduced and deployed solutions that scale from thousands of users to millions of users. I specialise in web server infrastructure, continuous integration and delivery solutions, workload automatisation and optimisation.
+    <div>
+        <header>
+            <h1 class="header">about me:</h1>
+        </header>
+        <div class="photo-section">
+            <v-img src="@/assets/profie_photo.jpg" class="profile-img"/>
+        </div>
+        <pre class="description-section">My name is Tomas. I am a Dev-Ops Engineer based in Lithuania. I am a seasoned expert that has introduced and deployed solutions that scale from thousands of users to millions of users. I specialise in web server infrastructure, continuous integration and delivery solutions, workload automatisation and optimisation.
     </pre>
-    <pre class="expertise-section">
+        <pre class="expertise-section">
       Experience:
       Dev-Ops Engineer at NFQ
       Feb 2019 - Present
@@ -68,52 +23,133 @@
       Certifications:
       Zend Certified PHP Engineer
       Issued Jan 2017
-      Credential ID: ZEND29620
+      Credential ID: <a target="_blank" href="https://www.zend-zce.com/en/yellow-pages/ZEND029620">ZEND29620</a>
     </pre>
-
-  </div>
-
+        <pre class="expertise-section-855">
+      Experience:
+      Dev-Ops Engineer at
+      NFQ
+      Feb 2019 - Present
+      PHP Programmer at
+      Enigma Software
+      Dec 2012 - Feb 2019
+        </pre>
+        <pre class="expertise-section-855">
+      Education:
+      Kaunas University
+      of Technology
+      BSc Information
+      Technology
+      2012-2016
+        </pre>
+        <pre class="expertise-section-855">
+      Certifications:
+      Zend Certified
+      PHP Engineer
+      Issued Jan 2017
+      Credential ID:
+      <a target="_blank" href="https://www.zend-zce.com/en/yellow-pages/ZEND029620">ZEND29620</a>
+    </pre>
+    </div>
 </template>
 
 <style>
-  .profile-img {
-    height: 300px;
-    width: 200px;
-    border-radius: 8px;
-    margin-left: auto;
-    margin-right: auto;
-    display: block;
-  }
-  .photo-section {
-    width: 27%;
-    float: left;
-    height: 100%;
-    margin-top: 30px;
-  }
-  .description-section {
-    width: 35%;
-    float: left;
-    height: 100%;
-    margin-top: 30px;
-    text-align: start;
-    padding: 0 20px 0 20px;
-    color: white;
-    white-space: pre-wrap;       /* Since CSS 2.1 */
-    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
-    white-space: -o-pre-wrap;    /* Opera 7 */
-    word-wrap: break-word;       /* Internet Explorer 5.5+ */
-  }
-  .expertise-section {
-    width: 35%;
-    float: left;
-    height: 100%;
-    margin-top: 30px;
-    border-left: 2px solid white;
-    text-align: start;
-    color: white;
-    white-space: pre-wrap;       /* Since CSS 2.1 */
-    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
-    white-space: -o-pre-wrap;    /* Opera 7 */
-    word-wrap: break-word;       /* Internet Explorer 5.5+ */
-  }
+    .profile-img {
+        height: 300px;
+        width: 200px;
+        border-radius: 8px;
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
+    }
+
+    .photo-section {
+        width: 27%;
+        float: left;
+        height: 100%;
+        margin-top: 30px;
+    }
+
+    .description-section {
+        width: 35%;
+        float: left;
+        height: 100%;
+        margin-top: 30px;
+        text-align: start;
+        padding: 0 20px 0 20px;
+        color: white;
+        white-space: pre-wrap; /* Since CSS 2.1 */
+        white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+        white-space: -o-pre-wrap; /* Opera 7 */
+        word-wrap: break-word; /* Internet Explorer 5.5+ */
+    }
+
+    .expertise-section {
+        width: 35%;
+        float: left;
+        height: 100%;
+        margin-top: 30px;
+        border-left: 2px solid white;
+        text-align: start;
+        color: white;
+        white-space: pre-wrap; /* Since CSS 2.1 */
+        white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+        white-space: -o-pre-wrap; /* Opera 7 */
+        word-wrap: break-word; /* Internet Explorer 5.5+ */
+    }
+
+    @media screen and (min-width:855px) {
+        .expertise-section-855 {
+            display: none;
+        }
+    }
+    @media screen and (max-width:855px) {
+        .expertise-section {
+            display: none;
+        }
+
+        .description-section {
+            width: 60%;
+            padding-top: 7%;
+            height: 300px;
+        }
+
+        .photo-section {
+            margin-left: 40px;
+        }
+
+        .expertise-section-855 {
+            width: 30%;
+            float: left;
+            height: 100%;
+            text-align: start;
+            color: white;
+            white-space: pre-wrap; /* Since CSS 2.1 */
+            white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+            white-space: -o-pre-wrap; /* Opera 7 */
+            word-wrap: break-word; /* Internet Explorer 5.5+ */
+            margin-left: 20px;
+        }
+    }
+    @media screen and (max-width:600px) {
+        .expertise-section {
+            display: initial;
+            width: 100%;
+            margin-left: 40px;
+            padding-top: 30px;
+            border: none;
+        }
+        .expertise-section-855 {
+            display: none;
+        }
+        .description-section {
+            border-bottom: 2px solid white;
+            width: 80%;
+            margin-left: 40px;
+            padding-bottom: 40px;
+        }
+        .photo-section {
+            width: 70%;
+        }
+    }
 </style>
