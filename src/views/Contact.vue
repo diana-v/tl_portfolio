b2e76a36bfe77dc412d8ab1f7b2b6a057678f4f7
Diana V <d.valaityte.97@gmail.com>
2020-01-07 21:15:46 +0200

    Added copy to clipboard functionality to the email address and styled the icons and the text.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,40 +1,41 @@
 <template>
     <a class="about">
         <h1 class="header">contact me:</h1>
-        <pre class="contact-section"><v-icon class="contact-icon" size="50">fab fa-github</v-icon><a target="_blank" class="contact-link" href="https://github.com/tomaslesc">GitHub</a></pre>
+        <v-icon class="contact-icon" size="50">fab fa-github</v-icon>
+        <pre class="contact-section"><a target="_blank" class="contact-link"
+                                        href="https://github.com/tomaslesc">GitHub</a></pre>
 
-        <pre class="contact-section"><v-icon class="contact-icon" size="50">fab fa-linkedin</v-icon><a target="_blank" class="contact-link" href="https://www.linkedin.com/in/tomaslesc/">LinkedIn</a></pre>
+        <v-icon class="contact-icon" size="50">fab fa-linkedin</v-icon>
+        <pre class="contact-section"><a target="_blank" class="contact-link"
+                                        href="https://www.linkedin.com/in/tomaslesc/">LinkedIn</a></pre>
 
-        <pre class="contact-section"><v-icon class="contact-icon" size="50">fas fa-envelope</v-icon><a class="contact-link" href="#" data-toggle="tooltip" title="Click to copy my email address to your clipboard">Gmail</a></pre>
+        <v-icon class="contact-icon" size="50">fas fa-envelope</v-icon>
+        <pre class="contact-section"><a class="contact-link" href="#" data-toggle="tooltip"
+                                        title="Click to copy my email address to your clipboard" v-on:click="clippy">Gmail</a></pre>
+        <input type="text" style="display:none" id="clipboard" value="tom@e0.lt">
     </a>
 </template>
 
 <script>
+    export default {
+        name: "app",
+        data: () => ({}),
+        methods: {
+            clippy: function () {
+                /* Get the text field */
+                var copyText = document.getElementById("clipboard");
+                //
+                // /* Select the text field */
+                copyText.select();
 
-    // function openTooltip() {
-    //     ('[data-toggle="tooltip"]').tooltip({ boundary: 'window' });
-    // }
-    // openTooltip();
-    // function copy_text() {
-    //     /* Get the text field */
-    //     var copyText = document.getElementById("copy-to-clipboard");
-    //
-    //     /* Select the text field */
-    //     copyText.select();
-    //     copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-    //
-    //     /* Copy the text inside the text field */
-    //     document.execCommand("copy");
-    //
-    //     // /* Alert the copied text */
-    //     // alert("Copied to clipboard!");
-    // }
-    // copy_text();
+                /* Copy the text inside the text field */
+                document.execCommand("copy");
 
-    // value="tom@e0.lt"
-    // id="copy-to-clipboard"
-    // data-toggle="tooltip"
-    // title="Click to copy my email address to your clipboard"
+                /* Alert the copied text */
+                alert("My email address has been copied to your clipboard!");
+            }
+        }
+    }
 </script>
 
 <style>
@@ -42,13 +43,18 @@
         font-size: 18px;
         margin-top: 50px;
         color: white;
+        float: left;
         white-space: pre-wrap; /* Since CSS 2.1 */
         white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
         white-space: -o-pre-wrap; /* Opera 7 */
         word-wrap: break-word; /* Internet Explorer 5.5+ */
     }
+
     .contact-icon {
-        padding-right: 20px;
+        padding-left: 35%;
+        float: left;
+        width: 50%;
+        margin-top: 40px;
     }
 
     .contact-link {
