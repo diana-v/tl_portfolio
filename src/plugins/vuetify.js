2ce327a7f9b34516c94506a5b6efdf2bfc083f29
Diana V <d.valaityte.97@gmail.com>
2020-01-09 21:13:55 +0200

    Added a contact form to the contact page with validation for the name, email and reason for contacting fields.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-import '@mdi/font/css/materialdesignicons.css'
-import '@fortawesome/fontawesome-free/css/all.css'
+import '@mdi/font/css/materialdesignicons.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import Vuelidate from 'vuelidate';
+// import required, maxLength, email from 'vuelidate/lib/validators';
 
 Vue.use(Vuetify);
+Vue.use(Vuelidate);
 
 export default new Vuetify({
   theme: {
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,25 +1,100 @@
 <template>
     <a class="about">
         <h1 class="header">contact me:</h1>
-        <v-icon class="contact-icon" size="50">fab fa-github</v-icon>
-        <pre class="contact-section"><a target="_blank" class="contact-link"
-                                        href="https://github.com/tomaslesc">GitHub</a></pre>
+        <div class="contact-ways">
+            <v-icon class="contact-icon" size="50">fab fa-github</v-icon>
+            <pre class="contact-section">
+                <a target="_blank" class="contact-link" href="https://github.com/tomaslesc">GitHub</a>
+            </pre>
 
-        <v-icon class="contact-icon" size="50">fab fa-linkedin</v-icon>
-        <pre class="contact-section"><a target="_blank" class="contact-link"
-                                        href="https://www.linkedin.com/in/tomaslesc/">LinkedIn</a></pre>
+            <v-icon class="contact-icon" size="50">fab fa-linkedin</v-icon>
+            <pre class="contact-section">
+                <a target="_blank" class="contact-link" href="https://www.linkedin.com/in/tomaslesc/">LinkedIn</a>
+            </pre>
 
-        <v-icon class="contact-icon" size="50">fas fa-envelope</v-icon>
-        <pre class="contact-section"><a class="contact-link" href="#" data-toggle="tooltip"
-                                        title="Click to copy my email address to your clipboard" v-on:click="clippy">Gmail</a></pre>
-        <input type="text" style="display:none" id="clipboard" value="tom@e0.lt">
+            <v-icon class="contact-icon" size="50">fas fa-envelope</v-icon>
+            <pre class="contact-section">
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <a class="contact-link" v-on:click="clippy" v-on="on">Gmail</a>
+                    </template>
+                <span>Click to copy my email address to your clipboard</span>
+                </v-tooltip>
+            </pre>
+            <input type="text" style="display:none" id="clipboard" value="tom@e0.lt">
+        </div>
+
+        <div class="contact-form">
+            <form>
+                <v-text-field
+                        v-model="name"
+                        :error-messages="nameErrors"
+                        :counter="10"
+                        label="Name"
+                        required
+                        @input="$v.name.$touch()"
+                        @blur="$v.name.$touch()"
+                ></v-text-field>
+                <v-text-field
+                        v-model="email"
+                        :error-messages="emailErrors"
+                        label="E-mail"
+                        required
+                        @input="$v.email.$touch()"
+                        @blur="$v.email.$touch()"
+                ></v-text-field>
+                <v-textarea
+                        v-model="placeholder"
+                        :rules="rules"
+                        required
+                        auto-grow
+                        rows="1"
+                        label="Reason for contacting"
+                ></v-textarea>
+                <v-btn class="mr-4" @click="submit">submit</v-btn>
+                <v-btn @click="clear">clear</v-btn>
+            </form>
+        </div>
     </a>
 </template>
 
 <script>
+    import {required, maxLength, email} from 'vuelidate/lib/validators'
+
     export default {
         name: "app",
-        data: () => ({}),
+        validations: {
+            name: {required, maxLength: maxLength(10)},
+            email: {required, email},
+        },
+
+        data: () => (
+            {
+                name: '',
+                email: '',
+                rules: [
+                    value => !!value || 'Required.',
+                    value => (value || '').length >= 20 || 'Min 20 characters',
+                ]
+            }
+        ),
+        computed: {
+            nameErrors() {
+                const errors = []
+                if (!this.$v.name.$dirty) return errors
+                !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+                !this.$v.name.required && errors.push('Name is required.')
+                return errors
+            },
+            emailErrors() {
+                const errors = []
+                if (!this.$v.email.$dirty) return errors
+                !this.$v.email.email && errors.push('Must be valid e-mail')
+                !this.$v.email.required && errors.push('E-mail is required')
+                return errors
+            },
+        },
+
         methods: {
             clippy: function () {
                 /* Get the text field */
@@ -32,18 +107,44 @@
                 document.execCommand("copy");
 
                 /* Alert the copied text */
-                alert("My email address has been copied to your clipboard!");
-            }
+                alert("My email address has been copied to your clipboard! I'll be waiting to hear from you.");
+            },
+            submit() {
+                this.$v.$touch()
+            },
+            clear() {
+                this.$v.$reset()
+                this.name = ''
+                this.email = ''
+                this.select = null
+            },
         }
-    }
+        };
 </script>
 
 <style>
-    .contact-section {
-        font-size: 18px;
-        margin-top: 50px;
-        color: white;
+    .contact-ways {
+        width: 40%;
         float: left;
+        align-content: start;
+        display: grid;
+        grid-template-columns: 40% 60%;
+        grid-template-rows: auto auto 100px;
+        margin-top: 40px;
+    }
+
+    .contact-form {
+        width: 50%;
+        float: left;
+        margin-top: 40px
+    }
+
+    .contact-section {
+        text-align: start;
+        width: 60%;
+        float: left;
+        font-size: 18px;
+        color: white;
         white-space: pre-wrap; /* Since CSS 2.1 */
         white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
         white-space: -o-pre-wrap; /* Opera 7 */
@@ -51,18 +152,28 @@
     }
 
     .contact-icon {
-        padding-left: 35%;
+        padding-left: 15%;
         float: left;
-        width: 50%;
-        margin-top: 40px;
     }
 
     .contact-link {
         text-decoration: none !important;
         color: white !important;
+        width: 70%;
     }
 
     .contact-link:hover {
         border-bottom: 3px solid white;
+    }
+
+    pre.contact-section:nth-child(6) {
+        height: 81px;
+        overflow: hidden;
+        display: ruby;
+        align-content: middle;
+    }
+
+    a.contact-link:nth-child(2) {
+        line-height: 100px;
     }
 </style>
