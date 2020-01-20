307cd89f5ae1b6c7cc0afb2417bc084997e62ba9
Diana V <d.valaityte.97@gmail.com>
2020-01-20 22:04:56 +0200

    Hover effects and styling when there are multiple entries. Compatability at all screen sizes.

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,17 +1,15 @@
 <template>
     <div class="blog">
         <h1 class="header">blog:</h1>
+        <div class="blog-wrap">
         <div class="blog-container">
             <v-card to="/blog/1" link hover
-                    class="ml-5 blog-card"
-                    max-width="400"
-            >
+                    class="blog-card">
                 <v-img
                         class="white--text align-end"
                         height="200px"
                         src="https://cdn.pixabay.com/photo/2018/09/03/22/05/programming-3652497_1280.jpg"
                 >
-
                 </v-img>
                 <v-card-title class="blog-card-title">First Blog Entry</v-card-title>
                 <v-card-subtitle class="pb-0 blog-card-subtitle">November 10 2020</v-card-subtitle>
@@ -19,6 +17,74 @@
                     <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin viverra turpis eget congue. Nulla a metus a mi venenatis placerat...</div>
                 </v-card-text>
             </v-card>
+        </div>
+
+        <div class="blog-container">
+            <v-card to="/blog/1" link hover
+                    class="blog-card">
+                <v-img
+                        class="white--text align-end"
+                        height="200px"
+                        src="https://cdn.pixabay.com/photo/2018/09/03/22/05/programming-3652497_1280.jpg"
+                >
+                </v-img>
+                <v-card-title class="blog-card-title">First Blog Entry</v-card-title>
+                <v-card-subtitle class="pb-0 blog-card-subtitle">November 10 2020</v-card-subtitle>
+                <v-card-text class="text--primary">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin viverra turpis eget congue. Nulla a metus a mi venenatis placerat...</div>
+                </v-card-text>
+            </v-card>
+        </div>
+        <div class="blog-container">
+            <v-card to="/blog/1" link hover
+                    class="blog-card">
+                <v-img
+                        class="white--text align-end"
+                        height="200px"
+                        src="https://cdn.pixabay.com/photo/2018/09/03/22/05/programming-3652497_1280.jpg"
+                >
+                </v-img>
+                <v-card-title class="blog-card-title">First Blog Entry</v-card-title>
+                <v-card-subtitle class="pb-0 blog-card-subtitle">November 10 2020</v-card-subtitle>
+                <v-card-text class="text--primary">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin viverra turpis eget congue. Nulla a metus a mi venenatis placerat...</div>
+                </v-card-text>
+            </v-card>
+        </div>
+
+        <div class="blog-container">
+            <v-card to="/blog/1" link hover
+                    class="blog-card">
+                <v-img
+                        class="white--text align-end"
+                        height="200px"
+                        src="https://cdn.pixabay.com/photo/2018/09/03/22/05/programming-3652497_1280.jpg"
+                >
+                </v-img>
+                <v-card-title class="blog-card-title">First Blog Entry</v-card-title>
+                <v-card-subtitle class="pb-0 blog-card-subtitle">November 10 2020</v-card-subtitle>
+                <v-card-text class="text--primary">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin viverra turpis eget congue. Nulla a metus a mi venenatis placerat...</div>
+                </v-card-text>
+            </v-card>
+        </div>
+
+        <div class="blog-container">
+            <v-card to="/blog/1" link hover
+                    class="blog-card">
+                <v-img
+                        class="white--text align-end"
+                        height="200px"
+                        src="https://cdn.pixabay.com/photo/2018/09/03/22/05/programming-3652497_1280.jpg"
+                >
+                </v-img>
+                <v-card-title class="blog-card-title">First Blog Entry</v-card-title>
+                <v-card-subtitle class="pb-0 blog-card-subtitle">November 10 2020</v-card-subtitle>
+                <v-card-text class="text--primary">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin viverra turpis eget congue. Nulla a metus a mi venenatis placerat...</div>
+                </v-card-text>
+            </v-card>
+        </div>
         </div>
     </div>
 </template>
@@ -37,12 +103,23 @@
     }
 
     .blog-container {
-        margin-left: 40px;
-        margin-top: 20px;
+        float: left;
+        margin: 20px 5%;
+        width: 40%;
+    }
+
+    @media screen and (max-width: 825px) {
+        .blog-container {
+            width: 80%;
+            margin: 0 auto;
+            padding: 20px;
+            float: unset;
+        }
     }
 
     .blog-card {
         text-decoration: none !important;
+        width: 600px;
     }
 
     .blog-card-title {
@@ -56,5 +133,13 @@
     .blog-card-subtitle {
         border-bottom: 3px solid #f08c49;
         width: 150px;
+    }
+
+    .blog-wrap:hover .blog-card {
+        opacity: .3;
+    }
+
+    .blog-wrap .blog-card:hover {
+        opacity: 1;
     }
 </style>
