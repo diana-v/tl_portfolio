f8f03bb75b68cb3b7aa34856d3e4fabfe3c4d479
tlescinskas <tomas.lescinskas@nfq.lt>
2020-09-02 22:07:12 +0300

    Updated content for main page and about

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,66 @@
 <template>
   <pre class="home-wrap">
-    <prism class="home-content" language="js">
+    <prism class="home-content" language="hcl">
       {{ content }}
     </prism>
   </pre>
 </template>
 
 <script>
-  export default {
-    data: () => (
-            {
-              content: '',
-              code: "\n" +
-                    "let tom = new DevOps('tom');\n" +
-                    "while(tom.awake()){\n" +
-                    "    tom.think();\n" +
-                    "    tom.consult();\n" +
-                    "    tom.work();\n" +
-                    "}"
-            }
-    ),
-    mounted() {
-      setTimeout(this.addMoreCode)
-    },
-    methods: {
-      addMoreCode: function () {
-        if (this.code === ""){
-          return
-        }
-        this.content += this.code[0];
-        this.code = this.code.substring(1);
-        setTimeout(this.addMoreCode, 100)
+export default {
+  data: () => ({
+    content: "",
+    code: `
+terraform init
+
+provider "cloudflare" {
+  version = "~> 2.0"
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
+  account_id = var.cloudflare_account_id
+}
+
+resource "cloudflare_zone" "e0lt" {
+ zone = "e0.lt"
+}
+
+resource "cloudflare_record" "root" {
+  zone_id = var.zone.e0lt
+  name    = ""
+  type    = "A"
+  ttl     = 1
+  proxied = true
+  value = "Secret..."
+}
+
+Infrastructure is up-to-date.
+
+Welcome.
+`
+  }),
+  mounted() {
+    setTimeout(this.addMoreCode);
+  },
+  methods: {
+    addMoreCode: function() {
+      if (this.code === "") {
+        return;
       }
+      this.content += this.code[0];
+      this.code = this.code.substring(1);
+      setTimeout(this.addMoreCode, 20);
     }
-  };
+  }
+};
 </script>
 
 <style>
-  .home-content {
-    background: transparent !important;
-  }
+.home-content {
+  background: transparent !important;
+}
 
-  .home-wrap {
-    margin: 10vh auto !important;
-    font-size: 20px !important;
-  }
- </style>
+.home-wrap {
+  margin: auto;
+  font-size: 1.9vh;
+}
+</style>
